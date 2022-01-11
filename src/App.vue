274dbb5df72c17cf2de03c88783162603fc26da1
Anand raja <meetanandns@gmail.com>
2022-01-11 10:01:40 +0000

    passing data back to parent

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
     <ul>
       <friend-contact v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name"
         :phone-number="friend.phone"
         :email-address="friend.email"
         :is-favorite="friend.favorite"
+        @toggle-favorite="toggleFavoriteStatus"
       ></friend-contact>
     </ul>
   </section>
@@ -32,10 +34,16 @@
             name: 'Julie Jones',
             phone: '09876 543 221',
             email: 'julie@localhost.com',
-            favorite: true
+            favorite: false
           },
         ],
       };
+    },
+    methods: {
+      toggleFavoriteStatus(friendID) {
+        const identifiedFrnd = this.friends.find(friend => friend.id === friendID);
+        identifiedFrnd.favorite = !identifiedFrnd.favorite;
+      }
     }
   };
 </script>
