a835b71f37e8e1d79527633d4fbb2b78f29e8983
Anand raja <meetanandns@gmail.com>
2022-01-11 07:32:08 +0000

    basic friend component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        friends: [
+          {
+            id: 'rajesh',
+            name: 'Rajesh Kumar',
+            phone: '01234 5678 991',
+            email: 'rajesh@localhost.com',
+          },
+          {
+            id: 'julie',
+            name: 'Julie Jones',
+            phone: '09876 543 221',
+            email: 'julie@localhost.com',
+          },
+        ],
+      };
+    }
+  };
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+          id: 'rajesh',
+          name: 'Rajesh Kumar',
+          phone: '01234 5678 991',
+          email: 'rajesh@localhost.com',
+        }
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
