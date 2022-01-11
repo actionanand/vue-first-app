d27681ac0b462caef50123c7cce1037c623c41b7
Anand raja <meetanandns@gmail.com>
2022-01-11 09:45:10 +0000

    dynamic props

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
       <h1>My Friends</h1>
     </header>
     <ul>
-      <friend-contact
-        name="Rajesh Kumar" phone-number="01234 5678 991" email-address="rajesh@localhost.com"
-      ></friend-contact>
-      <friend-contact
-        name="Julie Jones" phone-number="09876 543 221" email-address="julie@localhost.com"
+      <friend-contact v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.favorite"
       ></friend-contact>
     </ul>
   </section>
@@ -24,12 +25,14 @@
             name: 'Rajesh Kumar',
             phone: '01234 5678 991',
             email: 'rajesh@localhost.com',
+            favorite: true
           },
           {
             id: 'julie',
             name: 'Julie Jones',
             phone: '09876 543 221',
             email: 'julie@localhost.com',
+            favorite: true
           },
         ],
       };
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,37 +1,60 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
-    <button @click="toggleDetails"> {{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : ''}}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
-      <li><strong>Email:</strong>{{ emailAddress }}</li>
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAddress'
-  ],
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-          id: 'rajesh',
-          name: 'Rajesh Kumar',
-          phone: '01234 5678 991',
-          email: 'rajesh@localhost.com',
-        }
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
 };
 </script>
 
